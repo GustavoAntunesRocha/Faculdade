89f7c8b67a2365d8d6ea9cc46c6c58ff0427cda9
GustavoAntunesRocha <gustavo.antunes.rocha@outlook.com>
2021-03-15 11:13:10 -0300

    Exercicio 3, incompleto

--- Lista1/3.c
+++ Lista1/3.c
@@ -1,17 +1,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdio_ext.h>
-
+#include <string.h>
+void splitEspaces(char string[]);
+char  numeros[5];
 void main(int argc, char *argv[]){
-    int n = 0;
-    int a = 0;
-    int b = 0;
-    char entrada[5];
+    int n;
+    char entrada[10];
     scanf("%d",&n);
     __fpurge(stdin);
     gets(entrada);
-    printf("%s\n",entrada);
-    a = entrada[0];
-    b = entrada[4];
-    printf("%d, %d",a,b);
+    int i= 0;
+    int op1;
+    int op2;
+    char op;
+    printf("entrada: %s\n",entrada);
+    while(!strcmp(entrada[i], "\n")){
+       op1 += (int)entrada[i];
+       printf("Estágio 1: %d",op1);
+       if(entrada[i] == " "){
+          i++;
+          op = entrada[i];
+          printf("Estágio 2: %d",op);
+       }
+       i += 2;
+       while(!strcmp(entrada[i], "\n"))
+       {
+          op2 += (int)entrada[i];
+          printf("Estágio 3: %d",op2);
+          i++;
+       }
+       
+    }
+    printf("%d, %c, %d",op1,op,op2);
+    /*splitEspaces(entrada);
+    if(strcmp(numeros[1],"+")){
+       if((int)numeros[0] + (int)numeros[2] > n){
+          printf("overflow + \n");
+       } else{
+          printf("no overflow + \n");
+       }
+    }
+    if(strcmp(numeros[1],"x")){
+       if((int)numeros[0] * (int)numeros[2] > n){
+          printf("overflow x \n");
+       } else{
+          printf("no overflow x \n");
+       }
+    }
+    */
+}
+void splitEspaces(char string[]){
+   // Extract the first token
+   char * token = strtok(string, " ");
+   // loop through the string to extract all other tokens
+   int i = 0;
+   while( token != NULL ) {
+      numeros[i] = &token;
+      token = strtok(NULL, " ");
+      i++;
+   }
 }
