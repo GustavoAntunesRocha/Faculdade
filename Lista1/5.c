2ba84c4fad6c98ac87c2c290377728b6fac18599
GustavoAntunesRocha <gustavo.antunes.rocha@outlook.com>
2021-03-19 16:28:43 -0300

    Nova tentativa para shariff

--- Lista1/5.c
+++ Lista1/5.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int base4(int num);
-
+char aux[200];
 int main(int argc, char *argv[]) {
     int n;
     int i;
@@ -12,9 +13,13 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < n; i++) {
         scanf("%d", &numeros[i]);
     }
+    for(i = 0; i < 200; i++){
+        aux[i] = NULL;
+    }
     for (i = 0; i < n; i++) {
         base4(numeros[i]);
     }
+    printf("%s",aux);
     return 0;
 }
 
@@ -22,25 +27,21 @@ int base4(int num) {
     if (num < 4) {
         switch (num) {
             case 1:
-                printf("C");
-                printf("\n");
+                strcat(aux,"C\n");
                 break;
             case 2:
-                printf("G");
-                printf("\n");
+                strcat(aux,"G\n");
                 break;
             case 3:
-                printf("T");
-                printf("\n");
+                strcat(aux,"T\n");
                 break;
             case 0:
-                printf("A");
-                printf("\n");
+                strcat(aux,"A\n");
                 break;
         }
         return 0;
     }
-    int i, j;
+    int i;
     int quociente;
 
     i = 0;
@@ -57,25 +58,26 @@ int base4(int num) {
     }
     i++;
     resto[i] = quociente;
-
+    
+    
     for (; i >= 0; i--){
         if (resto[i] < 4 && num >= 4) {
             switch (resto[i]) {
                 case 1:
-                    printf("C");
+                    strcat(aux,"C");
                     break;
                 case 2:
-                    printf("G");
+                    strcat(aux,"G");
                     break;
                 case 3:
-                    printf("T");
+                    strcat(aux,"T");
                     break;
                 case 0:
-                    printf("A");
+                    strcat(aux,"A");
                     break;
             }
         }
     }
-    printf("\n");
+    strcat(aux,"\n");
     return 0;
 }
