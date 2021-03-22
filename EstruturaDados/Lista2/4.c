44081343e7139f272e577eccb350bc5ef78c34cf
GustavoAntunesRocha <gustavo.antunes.rocha@outlook.com>
2021-03-22 17:59:40 -0300

    Inicio exerc 4 lista 2 ED

---
+++ EstruturaDados/Lista2/4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int reverso(int n){
+    if(n / 10 == 0){
+        return n;
+    }
+    else{
+        
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",reverso(n));
+    return 0;
+}
